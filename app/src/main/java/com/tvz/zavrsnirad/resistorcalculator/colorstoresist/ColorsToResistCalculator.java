b3cc7547ac6eeecbffd3c0d2262fd330d80e0010
antoniokranjcina <54414989+antoniokranjcina@users.noreply.github.com>
2020-07-17 22:09:38 +0200

    design improvements

--- app/src/main/java/com/tvz/zavrsnirad/resistorcalculator/colorstoresist/ColorsToResistCalculator.java
+++ app/src/main/java/com/tvz/zavrsnirad/resistorcalculator/colorstoresist/ColorsToResistCalculator.java
@@ -1,9 +1,7 @@
 package com.tvz.zavrsnirad.resistorcalculator.colorstoresist;
 
-import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.util.Log;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
